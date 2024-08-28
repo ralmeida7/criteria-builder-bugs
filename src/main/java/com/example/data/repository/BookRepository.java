65d7fceebe47e682681759907a53238cbf0c4904
Roberto Almeida <ralmeida@bytesw.com>
2024-08-28 17:13:54 -0600

    jdbc implementation

--- src/main/java/com/example/data/repository/BookRepository.java
+++ src/main/java/com/example/data/repository/BookRepository.java
@@ -3,13 +3,14 @@ package com.example.data.repository;
 import com.example.data.bo.Book;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
-import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.jpa.JpaSpecificationExecutor;
 import io.micronaut.data.repository.jpa.criteria.QuerySpecification;
 
 import java.util.List;
 
-@Repository
+@JdbcRepository(dialect = Dialect.H2)
 public interface BookRepository extends io.micronaut.data.repository.CrudRepository<com.example.data.bo.Book, Integer>, JpaSpecificationExecutor<com.example.data.bo.Book> {
     @Override
     @NonNull List<Book> findAll(@Nullable QuerySpecification<Book> spec);
